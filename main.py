896d5d9b43d474e586c22f202defa248a470429b
MuspiMerol <wjj123345567@iCloud.com>
2022-03-13 10:05:51 +0800

    收工！

--- main.py
+++ main.py
@@ -39,6 +39,10 @@ screenx = 1440
 screeny = 2960
 height = 247
 clock = pg.time.Clock()
+dock_cover_add = pg.Surface((screenx, height), pg.SRCALPHA)
+dock_cover = pg.Surface((screenx, height), pg.SRCALPHA)
+dock_cover_add.fill((32, 32, 32))
+dock_cover.fill((255, 255, 255, 32))
 
 class Display:
     def __init__(self):
@@ -68,16 +72,18 @@ class Display:
             img[:, screeny-height:], (155,155), 35
         )
 
+    def add_dock_and_cover(self):
+        self.blur_dock()
+        self.surface.blit(dock_cover_add, (0, screeny - height), special_flags=pg.BLEND_RGB_ADD)
+        self.surface.blit(dock_cover, (0, screeny - height))
+        self.surface.blit(cover, (0, 0))
+
 
 down: pg.Surface
 up: pg.Surface
 cover = pg.image.load("assets/cover.png")
 display = Display()
 black = display.surface.copy()
-dock_cover_add = pg.Surface((screenx, height), pg.SRCALPHA)
-dock_cover = pg.Surface((screenx, height), pg.SRCALPHA)
-dock_cover_add.fill((32, 32, 32))
-dock_cover.fill((255, 255, 255, 32))
 
 
 def inspect_surface(surface=display.surface):
@@ -89,29 +95,84 @@ def do(x):
     with display as screen:
         pos_down = (x // 2 - screenx // 2, 0)
         pos_up = (x, 0)
+
         screen.blit(down, pos_down)
+
         black.fill([0, 0, 0, int(get_value(64, 0, (x/screenx)**2.5))])
         screen.blit(black, pos_down)
-        screen.blit(up, pos_up)
-        display.blur_dock()
-        screen.blit(dock_cover_add, (0, screeny-height), special_flags=pg.BLEND_RGB_ADD)
-        screen.blit(dock_cover, (0, screeny-height))
-        screen.blit(cover, (0, 0))
 
-def render():
-    total = 40
-    for _ in range(123):
-        for i in alive_it(range(total)):
-            do(get_value(screenx, 0, i / total))
-            # pg.image.save(display.surface, f"output/{i}.png")
-        for i in alive_it(range(total)):
-            do(get_value(0, screenx, i / total))
-            # pg.image.save(display.surface, f"output/{i}.png")
+        screen.blit(up, pos_up)
+        display.add_dock_and_cover()
+
+
+total = 40
+
+count = 0
+
+
+def render_in():
+    global count
+    for i in alive_it(range(total)):
+        do(get_value(screenx, 0, i / total))
+        pg.image.save(display.surface, f"output/{count + i}.png")
+
+    count += total
+
+def render_out():
+    global count
+    for i in alive_it(range(total)):
+        do(get_value(0, screenx, i / total))
+        pg.image.save(display.surface, f"output/{count + i}.png")
+
+    count += total
+
+def render_static(page):
+    global count
+    with display as screen:
+        screen.blit(page, (0,0))
+        display.add_dock_and_cover()
+    it = iter(alive_it(range(total)))
+    i = next(it)
+    pg.image.save(display.surface, f"output/{count + i}.png")
+    b = open(f"output/{count + i}.png", "rb").read()
+    for i in it:
+        open(f"output/{count + i}.png", "wb").write(b)
+
+    count += total
 
 
 if __name__ == '__main__':
     use(model)
-    down = pg.image.load("assets/首页.png")
-    up = pg.image.load("assets/参与.png")
-    render()
-    # inspect_surface()
+    page_1 = pg.image.load("assets/首页.png")
+    page_2 = pg.image.load("assets/发布.png")
+    page_3 = pg.image.load("assets/参与.png")
+    page_4 = pg.image.load("assets/量表参考.png")
+    page_5 = pg.image.load("assets/待办.png")
+    page_6 = pg.image.load("assets/我的.png")
+
+    render_static(page_1)
+
+    down, up = page_1, page_2
+    render_in()
+    render_static(page_2)
+    render_out()
+
+    down, up = page_1, page_3
+    render_in()
+    render_static(page_3)
+    render_out()
+
+    down, up = page_1, page_4
+    render_in()
+    render_static(page_4)
+
+    down, up = page_4, page_5
+    render_in()
+    render_static(page_5)
+
+    down, up = page_5, page_6
+    render_in()
+    render_static(page_6)
+
+    down, up = page_1, page_6
+    render_out()
